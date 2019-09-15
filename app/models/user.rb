1a865143c8bf5f4ea920627f96d407d089a8698a
Ellen Omoto <ellen@localwise.com>
2019-09-14 21:12:06 -0700

    EO - add method to user to check for admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :recoverable, :rememberable, :validatable
 
+  def is_admin?
+    self.admin == true
+  end
 end
