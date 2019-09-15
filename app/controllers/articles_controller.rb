fddb2fc1718456458f8b885356a903f2c8d855ec
Ellen Omoto <ellen@localwise.com>
2019-09-14 20:54:40 -0700

    EO - updated published_at after saves and set article controller methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,9 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.published.order(:published_at).page(params[:page]).per(params[:per])
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -8,4 +8,14 @@ class Article < ApplicationRecord
   }
 
   validates :title, :body, presence: true
+
+  after_save :set_published_at
+
+  def set_published_at
+    if self.draft? && self.published_at.present?
+      self.update_attributes(published_at: nil)
+    elsif self.published? && self.published_at.nil?
+      self.update_attributes(published_at: Time.now)
+    end
+  end
 end
