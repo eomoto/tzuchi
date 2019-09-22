097fb5945ced6e8cf5776e4dcbc61d294c38930a
Ellen Omoto <ellen@localwise.com>
2019-09-22 14:31:01 -0700

    EO - update schema and migration to remove body

--- db/migrate/20190915004350_create_articles.rb
+++ db/migrate/20190915004350_create_articles.rb
@@ -3,7 +3,6 @@ class CreateArticles < ActiveRecord::Migration[6.0]
     create_table :articles do |t|
       t.string :title
       t.text :description
-      t.text :body
       t.string :author
       t.integer :tags
       t.integer :status
